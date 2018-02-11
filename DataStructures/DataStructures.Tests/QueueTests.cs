2ef937ebc99a2cf8585926ea7d76e72cb80a7ce4
Nicholas Ruffing <nick@Nicholass-MacBook-Pro.local>
2018-02-10 21:46:45 -0500

    Add unit tests for queue

---
+++ DataStructures/DataStructures.Tests/QueueTests.cs
@@ -0,0 +1,63 @@
+﻿using Xunit;
+using DataStructures.Queue;
+using DataStructures.Exceptions;
+
+namespace DataStructures.Tests
+{
+    public class QueueTests
+    {
+        [Fact]
+        public void ConstructionTest()
+        {
+            IQueue<int> queue = new Queue<int>();
+            Assert.Equal(0, queue.Count);
+        }
+
+        [Fact]
+        public void EmptyPeekTest()
+        {
+            IQueue<int> queue = new Queue<int>();
+            Assert.Throws<CollectionEmptyException>(() => queue.Peek());
+        }
+
+        [Fact]
+        public void EmptyDequeueTest()
+        {
+            IQueue<int> queue = new Queue<int>();
+            Assert.Throws<CollectionEmptyException>(() => queue.Dequeue());
+        }
+
+        [Fact]
+        public void EnqueueTest()
+        {
+            IQueue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+
+            Assert.Equal(1, queue.Count);
+            Assert.Equal(1, queue.Peek());
+        }
+
+        [Fact]
+        public void PeekTest()
+        {
+            IQueue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+
+            Assert.Equal(1, queue.Peek());
+            Assert.Equal(1, queue.Count);
+        }
+
+        [Fact]
+        public void DequeueTest()
+        {
+            IQueue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+            Assert.Equal(2, queue.Count);
+            Assert.Equal(1, queue.Peek());
+            Assert.Equal(1, queue.Dequeue());
+            Assert.Equal(1, queue.Count);
+        }
+    }
+}
